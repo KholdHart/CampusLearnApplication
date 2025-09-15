dd2f36713aff31b0b7b71ad446aa4c12b46d76d8
Khold Hart <thefakeburning@gmail.com>
2025-09-15 21:05:16 +0200

    Login and Tutor

---
+++ CampusLearnApplication/Models/Tutor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampusLearnApplication.Models
+{
+    internal class Tutor
+    {
+        public int tutorID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string specialization { get; set; }
+
+        public void assignModule(int courseID)
+        {
+            //logic 
+        }
+
+        public void reviewSubmission(int )
+        {
+            //logic
+        }
+
+        public void recieveReport()
+    }
+}
